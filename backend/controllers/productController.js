2750e4e27592a1bf7197edc199e97c662a1d7e20
DESKTOP-TBTGG3E\ZXCV <jtiago360@gmail.com>
2023-02-08 16:53:21 +0000

    Started sctrucutre of backend with routes and controllers!

---
+++ backend/controllers/productController.js
@@ -0,0 +1,6 @@
+const getProducts = (req,res) => {
+    res.send("Handling product routes, eg searching for prodcuts")
+}
+
+module.exports = getProducts
+
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const apiRoutes = require("./routes/apiRoutes")
+
+app.use('/api',apiRoutes)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app;

5c25d71bdeea79b4d65b1a9e26c4129210699317
DESKTOP-TBTGG3E\ZXCV <jtiago360@gmail.com>
2023-02-15 13:20:06 +0000

    Added a couple of routes and logic to the product controller involving admin usage. Added inserting and deleting of images, local for now, later to be changed to a server.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const {getProducts, getProductById, getBestsellers} = require("../controllers/productController")
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+  getBestsellers,
+  adminGetProducts,
+  adminDeleteProduct,
+  adminCreateProduct,
+  adminUpdateProduct,
+  adminUpload,
+  adminDeleteProductImage,
+} = require("../controllers/productController");
 
-router.get("/category/:categoryName/search/:searchQuery", getProducts)
-router.get("/category/:categoryName", getProducts)
-router.get("/search/:searchQuery", getProducts)
-router.get("/", getProducts)
-router.get("/bestsellers", getBestsellers)
-router.get("/get-one/:id", getProductById)
+router.get("/category/:categoryName/search/:searchQuery", getProducts);
+router.get("/category/:categoryName", getProducts);
+router.get("/search/:searchQuery", getProducts);
+router.get("/", getProducts);
+router.get("/bestsellers", getBestsellers);
+router.get("/get-one/:id", getProductById);
 
-module.exports = router
+//admin routes:
+router.get("/admin", adminGetProducts);
+router.delete("/admin/:id", adminDeleteProduct);
+router.post("/admin", adminCreateProduct);
+router.put("/admin/:id", adminUpdateProduct);
+router.post("/admin/upload", adminUpload);
+router.delete("/admin/image/:imagePath/:productId", adminDeleteProductImage);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const fileUpload = require("express-fileupload")
 const port = 3000
 
-const apiRoutes = require("./routes/apiRoutes")
 app.use(express.json())
+app.use(fileUpload())
+
+const apiRoutes = require("./routes/apiRoutes")
 
 app.use('/api', apiRoutes)
 

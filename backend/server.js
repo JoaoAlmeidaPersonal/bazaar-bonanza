e65bc59c9312406adc62dc6538e6709e6bcd7173
DESKTOP-TBTGG3E\ZXCV <jtiago360@gmail.com>
2023-02-19 23:54:36 +0000

    Small configuration changes!

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,46 @@
-const express = require('express')
-const app = express()
-const fileUpload = require("express-fileupload")
-const cookieParser = require("cookie-parser")
-const port = 3000
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 5000;
 
-app.use(express.json())
-app.use(fileUpload())
-app.use(cookieParser())
-const apiRoutes = require("./routes/apiRoutes")
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
 
-app.use('/api', apiRoutes)
+const apiRoutes = require("./routes/apiRoutes");
+
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
+});
 
 // mongodb connection
 const connectDB = require("./config/db");
 connectDB();
 
+app.use("/api", apiRoutes);
+
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    console.error(error);
+  }
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.status(500).json({
+      message: error.message,
+      stack: error.stack,
+    });
+  } else {
+    res.status(500).json({
+      message: error.message,
+    })
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
